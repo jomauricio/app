299dcf3d4f3e06e2176f0f58daf3dffcea9eb45e
Jose Mauricio <grandemau@gmail.com>
2023-08-21 15:08:36 -0300

    Adicionaod componente Contador

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { View, Text, Button, Alert } from "react-native";
 // import MeuTexto from "./components/MeuTexto";
-import MinMax from "./components/MinMax";
+// import MinMax from "./components/MinMax";
+import Contador from "./components/Contador";
 import Estilo from "./estilo";
 
 
 export default () => (
     <View style={Estilo.App}>
-        <MinMax min={4} max={15}/>
+        <Contador inicial={10}/>
+        {/*<MinMax min={4} max={15}/>
         <MinMax min={8} max={30}/>
         <MinMax min={6} max={11}/>
-        {/* <Text>Primeiro App!</Text>
+         <Text>Primeiro App!</Text>
         <MeuTexto/>
         <Button
         title="Cadastro"
